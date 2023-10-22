4e4cfca88e8fb4da4fa303b0a03dbeb95a401924
NosovArtem <nosovartem1992@gmail.com>
2023-10-22 23:26:57 +0200

    add adapter

---
+++ src/main/java/com/nsv/soft/dev/lab/base/patterns/structural/adapter/JavaApp.java
@@ -0,0 +1,18 @@
+package com.nsv.soft.dev.lab.base.patterns.structural.adapter;
+
+public class JavaApp {
+    void create(){
+        System.out.println("Creating Java object...");
+    }
+    void read(){
+        System.out.println("Reading Java object...");
+
+    }
+    void update(){
+        System.out.println("Updating Java object...");
+    }
+
+    void delete(){
+        System.out.println("Deleting Java object...");
+    }
+}
